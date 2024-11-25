95f89a1b92cac980bcd8e92d6170d83c55f982ea
Aulia Rahman <auliaarahmann@gmail.com>
2024-11-25 20:39:21 +0700

    Fix: remove unuse text

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// Countdown Timer Script
+const countdownDate = new Date("December 22, 2024 00:00:00").getTime();
+
+const x = setInterval(function () {
+  const now = new Date().getTime();
+  const distance = countdownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown-timer").innerHTML =
+      "Hari Bahagia Telah Tiba!";
+  }
+}, 1000);
